798991b81ebd7ef2343fdfe93bbb5eabe26081bf
aleramosAR <aleramos.ok@gmail.com>
2021-04-11 07:19:59 -0300

    Entrega Express Server

--- ExpressServer.js
+++ ExpressServer.js
@@ -6,6 +6,7 @@ const app = express();
 const path = "productos.txt";
 let productos = [];
 
+// Carga de productos desde el TXT
 const cargarProductos = async () => {
   try {
     productos = await fs.promises.readFile(path, "utf-8");
@@ -17,14 +18,19 @@ const cargarProductos = async () => {
 cargarProductos();
 
 
+// Variables para guardar la cantidad de visualizaciones de Productos y Producto Random
 let visProds = 0;
 let visRandom = 0;
 
+// Conexion a server con callback avisando de conexion exitosa
 const server = app.listen(PORT, () => {
   console.log(`Ya me conecte al puerto ${PORT}`);
 })
 server.on('error', (error) => console.log('Hay un error'))
 
+// Ruta /items
+// Parseo el listado de productos para tener el listado y cantidad
+// Devuelvo listado de productos y cantidad
 app.get('/items', (req, res) => {
   visProds++;
   const prods = JSON.parse(productos);
@@ -35,6 +41,8 @@ app.get('/items', (req, res) => {
   res.send(JSON.stringify(resultado, null, 2))
 })
 
+// Ruta /item-random
+// Parseo el listado de productos y selecciono uno al azar.
 app.get('/item-random', (req, res) => {
   visRandom++;
   const prods = JSON.parse(productos);
@@ -45,13 +53,16 @@ app.get('/item-random', (req, res) => {
   res.send(JSON.stringify(resultado, null, 2))
 })
 
+// Ruta /visitas
+// Devuelvo un JSON con el total de visitas a Productos y Producto Random
 app.get('/visitas', (req, res) => {
   const resultado = {visitas: { items: visProds, item: visRandom }};
   res.header("Content-Type",'application/json');
   res.send(JSON.stringify(resultado, null, 2))
 })
 
-
+// Ruta /
+// Devuelvo un pequeño HTML dando la bienvenida a la aplicacion.
 app.get('/', (req, res) => {
   res.send("<h1>Bienvenidos al servidor express</h1>");
 })
